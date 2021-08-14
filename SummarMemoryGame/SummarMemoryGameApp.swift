4ae4a2867f03c1fcf4240b03ac4e24ab110cfc4d
kamimi01 <awakaru0122@gmail.com>
2021-08-15 00:17:23 +0900

    fix: アプリの起動画面の修正

--- SummarMemoryGame/SummarMemoryGameApp.swift
+++ SummarMemoryGame/SummarMemoryGameApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct SummarMemoryGameApp: App {
     var body: some Scene {
         WindowGroup {
-            GamePlayResult(gameResult: .constant(GameResult.even))
+            TopView()
         }
     }
 }
