26b3a7b6effef9fb8cd6b3d063fad32605bc9dc5
kamimi01 <awakaru0122@gmail.com>
2021-08-15 08:15:16 +0900

    fix: ファイル名の修正

--- SummarMemoryGame/GamePlay/ViewModel/FlyerListViewModel.swift
+++ SummarMemoryGame/GamePlay/ViewModel/FlyerListViewModel.swift
@@ -11,7 +11,7 @@ class FlyerListViewModel: ObservableObject {
     
     private var allImageList = ["flyer1", "flyer2", "flyer3", "flyer4", "flyer5",
                                 "flyer6", "flyer7", "flyer8", "flyer9", "flyer10",
-                                "flyer11", "flyer12","flyer13", "flyer14", "flyer15", "flyer16", "flyer17", "flyer18", "flyer19", "flyey20", "flyer21", "flyer22", "flyer23", "flyer24"]
+                                "flyer11", "flyer12","flyer13", "flyer14", "flyer15", "flyer16", "flyer17", "flyer18", "flyer19", "flyer20", "flyer21", "flyer22", "flyer23", "flyer24"]
     
     @Published var imageList: [String] = []
     
@@ -34,8 +34,8 @@ class FlyerListViewModel: ObservableObject {
     @Published var isGameOver = false
     
     init() {
-        createRandomOrder()
-//        createNotRandomOrder() // デバッグ用メソッド
+//        createRandomOrder()
+        createNotRandomOrder() // デバッグ用メソッド
     }
     
     private func createNotRandomOrder() {
