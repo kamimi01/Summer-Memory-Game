44c9da5874b75250c7995c3e9fb1b4b34d563699
kamimi01 <awakaru0122@gmail.com>
2021-08-14 13:37:07 +0900

    feat: 初期コミット

---
+++ SummarMemoryGame/SummarMemoryGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SummarMemoryGameApp.swift
+//  SummarMemoryGame
+//
+//  Created by Mika Urakawa on 2021/08/14.
+//
+
+import SwiftUI
+
+@main
+struct SummarMemoryGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
