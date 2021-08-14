5f85abee9f65ca6483d95137a877d31af5d1b94f
あもるふぁす <huzeei0701@gmail.com>
2021-08-14 17:18:05 +0900

    horror image will be shown

--- SummarMemoryGame/GameResult/GamePlayResult.swift
+++ SummarMemoryGame/GameResult/GamePlayResult.swift
@@ -8,14 +8,18 @@
 import SwiftUI
 
 struct GamePlayResult: View {
+    @State private var isShowing = false
     var body: some View {
         VStack(spacing:20){
             Text("Nanakoさん\n勝利！").font(.system(size: 50, weight:.bold, design:.default))
                 .padding(.vertical,130)
                 .multilineTextAlignment(.center)
+            
             Button("罰ゲーム"){
-                
-            }
+                isShowing = true
+            }.sheet(isPresented: $isShowing, content: {
+                ModalView()
+            })
             .frame(width: 250, height: 60, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
             .accentColor(.white)
             .background(Color.blue)
@@ -24,7 +28,6 @@ struct GamePlayResult: View {
             
             
             Button("スタート画面に戻る"){
-                
             }
             .frame(width: 250, height: 60, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
             .accentColor(.white)
@@ -38,5 +41,13 @@ struct GamePlayResult: View {
 struct GamePlayResult_Previews: PreviewProvider {
     static var previews: some View {
         GamePlayResult()
+    }
+}
+
+struct ModalView: View {
+    var body: some View {
+        let randomInt = Int.random(in: 1..<3)
+        let imageName = "horror"+String(randomInt)
+        Image(imageName).resizable()
     }
 }
