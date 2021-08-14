fb1cae859cd9b010cb0c0dc824c909d7ab533555
kamimi01 <awakaru0122@gmail.com>
2021-08-14 21:11:11 +0900

    feat: 神経衰弱ゲームの内部実装完了
    #3

---
+++ SummarMemoryGame/GamePlay/ViewModel/FlyerListViewModel.swift
@@ -0,0 +1,105 @@
+//
+//  FlyerListViewModel.swift
+//  SummarMemoryGame
+//
+//  Created by Mika Urakawa on 2021/08/14.
+//
+
+import Foundation
+
+class FlyerListViewModel: ObservableObject {
+    
+    @Published var imageList = ["flyer1", "flyer2", "flyer3", "flyer4", "flyer5",
+                                "flyer6", "flyer7", "flyer8", "flyer9", "flyer10",
+                                "flyer11", "flyer12","flyer1", "flyer2", "flyer3", "flyer4", "flyer5", "flyer6", "flyer7", "flyer8", "flyer9", "flyer10", "flyer11", "flyer12"]
+    
+    @Published var imageStatusList = Array(repeating: false, count: 24)
+    
+    @Published var selectedImageList: [String] = []
+    
+    @Published var isSecondTurn = false
+    
+    @Published var result = false
+    
+    @Published var alertText = ""
+    
+    @Published var pointForFirst = 0
+    
+    @Published var pointForSecond = 0
+    
+    @Published var winner: GameResult = GameResult.even
+    
+    @Published var isGameOver = false
+    
+    init() {}
+    
+    func updateImageList(selectedIndex: Int, selectedImage: String) {
+        imageStatusList[selectedIndex] = true
+        print("更新後のリスト：\(imageStatusList)")
+        
+        selectedImageList.append(selectedImage)
+        
+        print("更新後のリスト2：\(selectedImageList)")
+        
+        if selectedImageList.count == 2 {
+            // imageListの中身が全て空の場合は、ゲーム終了
+            if (imageList.allSatisfy{ $0.isEmpty == true }) {
+                isGameOver = true
+                
+                if pointForFirst == pointForSecond {
+                    winner = GameResult.even
+                } else if pointForFirst < pointForSecond {
+                    winner = GameResult.player2
+                } else {
+                    winner = GameResult.player1
+                }
+
+                return
+            }
+            
+            result = true
+            // カードが同じかどうか
+            print(selectedImageList[0])
+
+            // 同じカードだった場合
+            if selectedImageList[0] == selectedImageList[1] {
+                for _ in imageList {
+                    if let selectedIndexFor2 = imageList.index(where: {$0 == selectedImageList[0]}) {
+                        imageList[selectedIndexFor2] = ""
+                    }
+                }
+                
+                print("画像のリスト：\(imageList)")
+                alertText = "同じチラシです！"
+                
+                // 得点の増加
+                if !isSecondTurn {
+                    pointForFirst = pointForFirst + 1
+                } else {
+                    pointForSecond = pointForSecond + 1
+                }
+            } else {
+                // 異なるカードだった場合
+                alertText = "違うチラシです..."
+            }
+            
+            // 選択されたカードのリストを空に戻す
+            selectedImageList = []
+        }
+
+        print("結果は：\(result)")
+    }
+    
+    func switchTurn() {
+        // 選択状態をリセットする
+        selectedImageList = []
+
+        // 全て裏に戻す
+        imageStatusList = Array(repeating: false, count: 24)
+
+        // 次の人のターンに切り替える
+        isSecondTurn = !isSecondTurn
+        
+        print("更新後のリスト3：\(selectedImageList)")
+    }
+}
