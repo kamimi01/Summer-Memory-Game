696fc2570b655e412c5f9e76e6bf4600925a31eb
kamimi01 <awakaru0122@gmail.com>
2021-08-21 13:33:39 +0900

    feat: アカウント情報の追加

---
+++ SummarMemoryGame/AccountInfo/AboutThisAppView.swift
@@ -0,0 +1,20 @@
+//
+//  AboutThisApp.swift
+//  SummarMemoryGame
+//
+//  Created by Mika Urakawa on 2021/08/18.
+//
+
+import SwiftUI
+
+struct AboutThisAppView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AboutThisAppView_Previews: PreviewProvider {
+    static var previews: some View {
+        AboutThisAppView()
+    }
+}
---
+++ SummarMemoryGame/AccountInfo/AccountInfoListView.swift
@@ -0,0 +1,74 @@
+//
+//  AccountInfoListView.swift
+//  SummarMemoryGame
+//
+//  Created by Mika Urakawa on 2021/08/18.
+//
+
+import SwiftUI
+
+struct AccountInfoListView: View {
+    
+    private let screen = UIScreen.main.bounds
+    
+    var body: some View {
+        
+        NavigationView {
+            accountList
+        }
+        
+    }
+}
+
+extension AccountInfoListView {
+    private var accountList: some View {
+        
+        ZStack {
+            Image("background3")
+                .resizable()
+                .edgesIgnoringSafeArea(.all)
+                .aspectRatio(contentMode: .fill)
+            
+            VStack(spacing: 0) {
+                Spacer()
+                    .frame(height: 80)
+                
+                NavigationLink(destination: AboutThisAppView()) {
+                    listCell(title: "このアプリについて")
+                }
+                
+                Spacer()
+            }
+            .frame(height: screen.height)
+            .navigationBarTitle("アカウント情報")
+            .navigationBarTitleDisplayMode(.inline)
+        }
+    }
+    
+    private func listCell(title: String) -> some View {
+        HStack(spacing: 0) {
+            Spacer()
+                .frame(width: 20)
+            
+            Text(title)
+                .font(.title2)
+                .foregroundColor(Color.black)
+            
+            Spacer()
+            
+            Image(systemName: "chevron.right")
+                .foregroundColor(Color.gray)
+            
+            Spacer()
+                .frame(width: 20)
+        }
+        .frame(width: screen.width, height: screen.height * 0.07, alignment: .leading)
+        .background(Color.white)
+    }
+}
+
+struct AccountInfoListView_Previews: PreviewProvider {
+    static var previews: some View {
+        AccountInfoListView()
+    }
+}
